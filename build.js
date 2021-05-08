9fa73daaba17165e174181271aca5d84e1168e45
mineppl12 <49873328+mineppl12@users.noreply.github.com>
2021-05-08 16:37:46 +0900

    Update build.js

--- build.js
+++ build.js
@@ -25,6 +25,6 @@ function ejs2html({ path, outPath, data, options }) {
 }
 
 ejs2html({
-  path: `${__dirname}`,
+  path: `${__dirname}/views/index.html`,
   outPath: `${__dirname}/public/index.html`
 });
